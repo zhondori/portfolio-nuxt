11b1551e073dbab0f766f02bc4302368549e8108
Alisher Usmonov <83527629+Alisher-Usmonov@users.noreply.github.com>
2023-02-06 14:39:43 +0500

    feat: dev branch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@nuxtjs/tailwindcss"
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap",
+        },
+      ],
+    },
+  },
+  css: [
+    "@/assets/css/tailwind.css"
   ]
-})
+
+});
