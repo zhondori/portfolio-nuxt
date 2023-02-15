8146a9a2f6da15c2cb437d20c0a6340b5445bc57
Alisher Usmonov <83527629+Alisher-Usmonov@users.noreply.github.com>
2023-02-15 10:05:27 +0500

    feat: completed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,11 +13,74 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap",
         },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+      ],
+      title: "Alisher Usmonov",
+      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+      meta: [
+        {
+          name: "description",
+          content: "Frontend Developer",
+        },
+        {
+          name: "og:type",
+          content: "website",
+        },
+        {
+          name: "og:url",
+          content: "https://alisher.netlify.app",
+        },
+        {
+          name: "og:title",
+          content: "Alisher Usmonov",
+        },
+        {
+          name: "og:description",
+          content: "Frontend Developer",
+        },
+        {
+          name: "og:image",
+          content: "/avatar.png",
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          name: "twitter:url",
+          content: "https://alisher.netlify.app/",
+        },
+        {
+          name: "twitter:title",
+          content: "Alisher Usmonov",
+        },
+        {
+          name: "twitter:description",
+          content: "Frontend Developer",
+        },
+        {
+          name: "og:image",
+          content: "/avatar.png",
+        },
       ],
     },
   },
-  css: [
-    "@/assets/css/tailwind.css"
-  ]
-
 });
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#ffffff",
+          dark: "rgb(31 41 55)",
+        },
+        text: {
+          light: "#ffffff",
+          dark: "rgb(31 41 55)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
